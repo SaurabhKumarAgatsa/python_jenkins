d2bd13cac96a6c2a05fff2d832175eabdee24481
Agatsa <agatsa@Agatsas-MacBook-Air.local>
2024-04-11 11:49:17 +0530

    modify

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-print("HELLO WORLD, HAPPY CODING :) ")
-print("DEPLOYMENT DONE ,CI/CD")
+def hello():
+    print("HELLO WORLD, HAPPY CODING :) ")
+    print("DEPLOYMENT DONE ,CI/CD")
+    return "DEPLOYMENT COMPLETED !!!!"
