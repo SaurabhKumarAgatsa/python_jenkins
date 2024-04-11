e93983f849bda4d8a73349bd0f09fdb4fff2d33a
Agatsa <agatsa@Agatsas-MacBook-Air.local>
2024-04-11 16:46:08 +0530

    updated

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,13 +6,6 @@ pipeline {
         sh 'python3 --version'
       }
     }
-    stage('Setup') {
-      steps {
-        sh 'python3 -m venv env' // Create a virtual environment
-        sh 'source env/bin/activate' // Activate the virtual environment
-        sh 'pip install -r requirements.txt' // Install dependencies
-      }
-    }
     stage('main') {
       steps {
         sh 'python3 main.py'
--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-# def hello():
-#     print("HELLO WORLD, HAPPY CODING :) ")
-#     print("DEPLOYMENT DONE ,CI/CD")
-#     print("Good dhdhdh")
-#     return "DEPLOYMENT COMPLETED !!!!"
-
-# hello()
-
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
 def hello():
-    return 'Hello, World!'
+    print("HELLO WORLD, HAPPY CODING :) ")
+    print("DEPLOYMENT DONE ,CI/CD")
+    print("Good dhdhdh")
+    return "DEPLOYMENT COMPLETED !!!!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+hello()
