b05a00e0200c89f0c083b61b956730da57270859
Agatsa <agatsa@Agatsas-MacBook-Air.local>
2024-04-11 11:59:21 +0530

    add prioudicaly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def hello():
     print("HELLO WORLD, HAPPY CODING :) ")
     print("DEPLOYMENT DONE ,CI/CD")
-    print("Good")
+    print("Good dhdhdh")
     return "DEPLOYMENT COMPLETED !!!!"
 
 hello()
