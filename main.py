a6bd2723f519a2782fdd906837856b9775d35119
Agatsa <agatsa@Agatsas-MacBook-Air.local>
2024-04-12 10:53:17 +0530

    changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def hello():
     print("HELLO WORLD, HAPPY CODING :) ")
     print("DEPLOYMENT DONE ,CI/CD")
-    print("Good dhdhdh")
+    print("Good dhdhdh- changes")
     return "DEPLOYMENT COMPLETED !!!!"
 
 hello()
